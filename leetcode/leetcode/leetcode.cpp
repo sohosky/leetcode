ae461a941f228b6093e8677ba764644b52fe4643
SuperSKY <sky.chen@emc.com.tw>
2022-02-15 13:33:17 +0800

    first commit

---
+++ leetcode/leetcode/leetcode.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	int ret = 0;
+	char ch;
+	wprintf_s(L"=========LeeCode SKY=======\n");
+	scanf("%c", &ch);
+	switch (ch)
+	{
+	case 0:
+		break;
+	case 1:
+		wprintf_s(L"====Two Sum====\n");
+		break;
+	}
+
+	return ret;
+}
